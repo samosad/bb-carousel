952c3dc9327bd0566164d613bcd4033e3699bad8
Aleksey Tabakman <samosad.ru@gmail.com>
2014-03-24 01:12:28 +0400

    init

---
+++ app/scripts/bbcarousel.js
@@ -0,0 +1,128 @@
+/* Backbone Carousel
+ * Source: https://github.com/samosad/bb-carousel
+ *
+ * Created by Aleksey Tabakman (@samosad)
+ * Released under the [MIT license](http://makesites.org/licenses/MIT)
+ */
+
+(function($, _, Backbone) {
+    var BBCarousel = Backbone.View.extend({
+
+        events: {
+            'click .prev': 'clickPrev',
+            'click .next': 'clickNext',
+            'click .nav a': 'clickNav'
+        },
+
+        initialize: function(options) {
+            var self = this,
+                options = options || {};
+            this.$el.addClass('bbcarousel');
+            this.position = 0;
+            if (options.template) {
+                $.get(options.template, function(data) {
+                    self.template = _.template(data);
+                    self.render();
+                });
+            } else {
+                self.render();
+            }
+        },
+
+        render: function() {
+            var self = this;
+            if (typeof this.template === 'function') {
+                this.$el.html(this.template({
+                    'slides': this.collection.toJSON()
+                }));
+            }
+            // slight delay to let the DOM rest
+            setTimeout(function() {
+                self.$slides = self.$el.find('.slide');
+                self.$wrapper = self.$el.find('.slides');
+                self.slidesWidth = self.$slides.width();
+                self.slidesNum = self.$slides.length;
+                self.showSlide(0);
+            }, 100);
+            return this;
+        },
+
+        clickPrev: function(e) {
+            e.preventDefault();
+            if (e.target.classList.contains('disabled')) {
+                return false;
+            }
+            var next = $(this.el).find('.slide.active').prev().index();
+            if (next > -1) {
+                this.showSlide(next);
+            }
+        },
+
+        clickNext: function(e) {
+            e.preventDefault();
+            if (e.target.classList.contains('disabled')) {
+                return false;
+            }
+
+            var prev = this.$el.find('.slide.active').next().index();
+            if (prev > -1) {
+                this.showSlide(prev);
+            }
+        },
+
+        clickNav: function(e) {
+            e.preventDefault();
+            if (e.target.classList.contains('active')) {
+                return false;
+            }
+            this.showSlide($(e.target).parent().index());
+        },
+
+        showSlide: function(num) {
+            // console.log('showSlide #' + num);
+            this.position = -1 * this.$slides.width() * num;
+            this.$wrapper.css('margin-left', this.position + 'px');
+            this.$slides.removeClass('active');
+            this.$slides[num].classList.add('active');
+            this.updateArrows().updateNav();
+        },
+
+        updateArrows: function() {
+            this.$el.find('.arrow').removeClass('disabled');
+            if (this.position === 0) {
+                this.$el.find('.arrow.prev').addClass('disabled');
+            } else if (this.position === -1 * this.slidesWidth * (this.slidesNum - 1)) {
+                this.$el.find('.arrow.next').addClass('disabled');
+            }
+            return this;
+        },
+
+        updateNav: function() {
+            var $items = this.$el.find('.nav li'),
+                activeSlideNum = this.$el.find('.slide.active').index();
+            $items.removeClass('active');
+            $items[activeSlideNum].classList.add('active');
+            return this;
+        }
+    });
+
+    // Support module loaders
+    if (typeof module === 'object' && module && typeof module.exports === 'object') {
+        // Expose as module.exports in loaders that implement CommonJS module pattern.
+        module.exports = BBCarousel;
+    } else {
+        // Register as a named AMD module, used in Require.js
+        if (typeof define === 'function' && define.amd) {
+            define([], function() {
+                return BBCarousel;
+            });
+        }
+    }
+    // If there is a window object, that at least has a document property
+    if (typeof window === 'object' && typeof window.document === 'object') {
+        window.Backbone = Backbone;
+        window.BBCarousel = BBCarousel;
+    }
+
+
+})(this.jQuery, this._, this.Backbone);
---
+++ app/scripts/main.js
@@ -0,0 +1,62 @@
+/*global bbCarousel, $*/
+
+
+window.APP = {
+    Models: {},
+    Collections: {
+        slides: new Backbone.Collection([
+            {
+                'src': '../images/7.jpg',
+                'label': 'Slide 7'
+            },
+            {
+                'src': '../images/6.jpg',
+                'label': 'Slide 6'
+            },
+            {
+                'src': '../images/5.jpg',
+                'label': 'Slide 5'
+            },
+            {
+                'src': '../images/4.jpg',
+                'label': 'Slide 4'
+            },
+            {
+                'src': '../images/3.jpg',
+                'label': 'Slide 3'
+            },
+            {
+                'src': '../images/2.jpg',
+                'label': 'Slide 2'
+            },
+            {
+                'src': '../images/1.jpg',
+                'label': 'Slide 1'
+            }
+        ])
+    },
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from main.js!');
+
+        // static carousel
+        APP.Views.carousel = new BBCarousel({
+            el: '#carousel'
+        });
+
+        // dinamic carousel
+        APP.Views.carousel2 = new BBCarousel({
+            el: '#carousel2',
+            collection: APP.Collections.slides,
+            template: '../templates/bbcarousel-template.html'
+        });
+        APP.Views.carousel2.render();
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    APP.init();
+});
