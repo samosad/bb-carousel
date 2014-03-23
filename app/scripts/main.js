3bfee417782aaffc415d1af2584b746537b3ecac
Aleksey Tabakman <samosad.ru@gmail.com>
2014-03-24 02:07:01 +0400

    new images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,32 +6,16 @@ window.APP = {
     Collections: {
         slides: new Backbone.Collection([
             {
-                'src': 'images/7.jpg',
+                'src': 'http://www.nature.org/cs/groups/webcontent/@photopublic/documents/media/mama-and-baby-bears-600x400.jpg',
                 'label': 'Slide 7'
             },
             {
-                'src': 'images/6.jpg',
+                'src': 'http://cocktailswithmom.com/wp-content/uploads/2011/11/Coca-Cola-Polar-Bear-Small-600x400.jpg',
                 'label': 'Slide 6'
             },
             {
-                'src': 'images/5.jpg',
+                'src': 'http://static.environmentalgraffiti.com/sites/default/files/images/polar-bear-swimming.img_assist_custom-600x400.jpg',
                 'label': 'Slide 5'
-            },
-            {
-                'src': 'images/4.jpg',
-                'label': 'Slide 4'
-            },
-            {
-                'src': 'images/3.jpg',
-                'label': 'Slide 3'
-            },
-            {
-                'src': 'images/2.jpg',
-                'label': 'Slide 2'
-            },
-            {
-                'src': 'images/1.jpg',
-                'label': 'Slide 1'
             }
         ])
     },
